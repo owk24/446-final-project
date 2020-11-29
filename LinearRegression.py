ac694b380599d212f5c57030c002ecb19a034bec
owk24 <cnowenkim@gmail.com>
2020-11-29 15:53:12 -0500

    change supervised model of interest to logistic regression

--- LinearRegression.py
+++ LinearRegression.py
@@ -5,19 +5,17 @@ pd.set_option('display.width', 1000)
 
 import numpy as np
 
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, classification_report
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = pd.read_csv("./Data/ModelData.csv")
 
-train, test = train_test_split(data, test_size=0.3)
+train, test = train_test_split(data, test_size=0.2)
 
-decisionTreeClassifier = DecisionTreeClassifier(min_samples_split=100)
 features = ["PERIOD", "MINUTES_REMAINING", "SHOT_TYPE", "SHOT_DISTANCE", "YEAR", "MONTH",
             "Back Court(BC)", "Center(C)", "Left Side Center(LC)", "Left Side(L)",
             "Right Side Center(RC)", "Right Side(R)",
@@ -40,9 +38,17 @@ yTrain = train[target]
 xTest = test[features]
 yTest = test[target]
 
-decisionTreeClassifier.fit(xTrain, yTrain)
+logisticRegressor = LogisticRegression(max_iter=500)
 
-yPrediction = decisionTreeClassifier.predict(xTest)
-score = accuracy_score(yTest, yPrediction)*100 # closer to %100
-f1Score = f1_score(yTest, yPrediction) # closer to 1
-print("Accuracy using Decision Tree: {}. F1 score is {}".format(score, f1Score))
+logisticRegressor.fit(xTrain, yTrain.values.ravel())
+
+yPrediction = logisticRegressor.predict(xTest)
+
+print(classification_report(yTest, yPrediction))
+print("The accuracy of the logistic model is: {}".format(accuracy_score(yTest, yPrediction)))
+
+coefficientInfo = dict(zip(features, logisticRegressor.coef_.transpose().tolist()))
+coefficientInfo["INTERCEPT"] = logisticRegressor.intercept_.tolist()
+print(coefficientInfo)
+
+
